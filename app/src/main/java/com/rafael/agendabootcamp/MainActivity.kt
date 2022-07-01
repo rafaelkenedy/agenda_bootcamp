4e20392938d8045fcb686171b043ecb2af5b5d0e
rafaelkenedy <k.rafaelalves@gmail.com>
2022-06-30 22:33:46 -0300

    Finalizado

--- app/src/main/java/com/rafael/agendabootcamp/MainActivity.kt
+++ app/src/main/java/com/rafael/agendabootcamp/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.rafael.agendabootcamp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CalendarContract
+import android.provider.CalendarContract.Events.*
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSetEvent = findViewById(R.id.set_event) as Button
+        btnSetEvent.setOnClickListener {
+            val intent = Intent(Intent.ACTION_INSERT)
+                .setData(CONTENT_URI)
+                .putExtra(TITLE, "Bootcamp Santander!")
+                .putExtra(EVENT_LOCATION, "online")
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, System.currentTimeMillis())
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, System.currentTimeMillis() + (3600 * 1000))
+
+            startActivity(intent)
+        }
     }
 }
